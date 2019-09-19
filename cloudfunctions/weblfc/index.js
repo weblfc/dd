f5106e9d61aa2e0c1cdfb1edf1d6d4b10acf253b
weblfc <951562762@qq.com>
2019-09-20 01:07:06 +0800

    1:06

--- cloudfunctions/weblfc/index.js
+++ cloudfunctions/weblfc/index.js
@@ -1,16 +1,15 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-
 cloud.init()
-
-// 云函数入口函数
+//1:引入request-promise库
+const rp = require("request-promise");
+//2:创建url
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
+  var url = `http://127.0.0.1:5000/api/v1/food/get_food_list?per_page=${event.per_page}&page=${event.page}&class_name=${event.class_name}`
+  //3:发送ajax
+  return rp(url).then(res => {
+    return res;
+  }).catch(err => {
+    console.log(err);
+  })
 }
